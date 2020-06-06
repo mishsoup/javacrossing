c8890385b05f0f4f50fdd59f13993c4ebcb46a6b
Michelle Lei <theoriginalmish@gmail.com>
2020-06-06 01:59:34 -0700

    rewrote the whole parser, kind of works now with @override and abunch of other cases

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -4,77 +4,115 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
+
     private final String importString = "import parser.OutputCreator; \n";
-    // this regex is taken from Stackoverflow post
-    // https://stackoverflow.com/questions/68633/regex-that-will-match-a-java-method-declaration
-    private final String functionRegex = "(?:(?:public|private|protected|static|final|native|synchronized|transient)+\\s+)+[$_\\w<>\\[\\]\\s]*\\s+[\\$_\\w]+\\([^\\)]*\\)?\\s*\\{?[^\\}]*\\}?";
+    // regex taken from Stackoverflow post
+    // https://stackoverflow.com/questions/37403641/regex-to-fetch-the-correct-java-class-name
+    private final String regexForClass = "(?<=\\n|\\A)(?:public\\s)?(class|enum|abstract class)";
+    // regex taken from Stackoverflow post
+    // https://stackoverflow.com/questions/47663648/javascript-regex-getting-function-name-from-string
+    private final String regexForFunc= "([a-zA-Z_{1}][a-zA-Z0-9_]+)(?=\\()";
 
     public String parseJavaFile(String javaString) {
         // if the java passed in is an interface then no modification required
         if (checkIfInterface(javaString)) {
             return javaString;
         }
-        String className = findClassName(javaString);
-        StringBuilder outputString = new StringBuilder(importString);
-        String[] arrOfJavaWOFunc = javaString.split(functionRegex);
-        // append all the non function declarations
-        for (int i = 0; i < arrOfJavaWOFunc.length - 1; i++) {
-            outputString.append(arrOfJavaWOFunc[i]);
-        }
-        // process the function declarations
-        Pattern pattern = Pattern.compile(functionRegex);
-        Matcher m = pattern.matcher(javaString);
-        while (m.find()) {
-            String funcName = findFunctionName(m.group(0));
-            String injectedFunc = injectToFunc(m.group(0), className, funcName);
-            outputString.append("\n\n"  + injectedFunc);
-        }
+        // split into string to parse each line
+        String javaStringAry[] = javaString.split("\\r?\\n");
+        String outputString = processEachLine(javaStringAry);
+        System.out.println(outputString);
+        return outputString;
+    }
 
-        outputString.append(arrOfJavaWOFunc[arrOfJavaWOFunc.length - 1]);
-        System.out.println(outputString.toString());
+    private String processEachLine(String[] javaStringAry) {
+        StringBuilder outputString = new StringBuilder("");
+        String className = "Class name not found";
+        String funcName = "Function name not found";
+        Boolean hasReturn = false;
+        int countOfBracket = 0;
+        Boolean stillInFunCall = false;
+        for (int i = 0; i < javaStringAry.length; i++) {
+            String currentLine = javaStringAry[i];
+            Pattern pattern = Pattern.compile(regexForClass);
+            Matcher classM = pattern.matcher(currentLine);
+            Pattern patternFunc = Pattern.compile(regexForFunc);
+            Matcher funcM = patternFunc.matcher(currentLine);
+            if (classM.find()) {
+                className = findClassName(currentLine);
+                outputString.append(importString + "\n");
+                outputString.append(currentLine + "\n");
+            } else if (funcM.find() && !stillInFunCall && !currentLine.contains("=")) {
+                stillInFunCall = true;
+                funcName = findFunctionName(currentLine);
+                if (countOfBracket == 0 && currentLine.contains("{")) {
+                    // injecting code to where the first { is found
+                    currentLine = currentLine.replaceFirst("\\{", "{ \n" +
+                            "        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();\n" +
+                            "        outputCreator.addJSON(\""+className+"\", \"Start_\" + \""+funcName+"\");\n");
+
+                }
+                if (currentLine.contains("return")) {
+                    currentLine = currentLine.replaceAll(
+                            "return",
+                            "outputCreator.addJSON(\""+className+"\", \"End_\" + \""+funcName+"\");\n " +
+                                    "       return");
+                    hasReturn = true;
+                }
+                countOfBracket += (int) currentLine.chars().filter(ch -> ch == '{').count();
+                countOfBracket -= (int) currentLine.chars().filter(ch -> ch == '}').count();
+                if (!hasReturn && countOfBracket == 0) {
+                    int pos = currentLine.lastIndexOf('}');
+                    currentLine = currentLine.substring(0,pos) +
+                            "    outputCreator.addJSON(\""+className+"\", \"End_\" + \""+funcName+"\");\n"
+                            + currentLine.substring(pos+1)
+                            + "    } \n";
+                }
+                // end of function parsing
+                if (countOfBracket == 0 ){
+                    stillInFunCall = false;
+                    hasReturn = false;
+                }
+                outputString.append(currentLine + "\n");
+            } else if (stillInFunCall){
+                if (countOfBracket == 0 && currentLine.contains("{")) {
+                    // injecting code to where the first { is found
+                    currentLine = currentLine.replaceFirst("\\{", "{ \n" +
+                            "        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();\n" +
+                            "        outputCreator.addJSON(\""+className+"\", \"Start_\" + \""+funcName+"\"); \n");
+
+                }
+                if (currentLine.contains("return")) {
+                    currentLine = currentLine.replaceAll(
+                            "return",
+                            "outputCreator.addJSON(\""+className+"\", \"End_\" + \""+funcName+"\");\n " +
+                                    "       return");
+                    hasReturn = true;
+                }
+                countOfBracket += (int) currentLine.chars().filter(ch -> ch == '{').count();
+                countOfBracket -= (int) currentLine.chars().filter(ch -> ch == '}').count();
+                if (!hasReturn && countOfBracket == 0) {
+                    int pos = currentLine.lastIndexOf('}');
+                    currentLine = currentLine.substring(0,pos) +
+                            "    outputCreator.addJSON(\""+className+"\", \"End_\" + \""+funcName+"\");\n"
+                            + currentLine.substring(pos+1)
+                            + "    } \n";
+                }
+                // end of function parsing
+                if (countOfBracket == 0 ){
+                    stillInFunCall = false;
+                    hasReturn = false;
+                }
+                outputString.append(currentLine + "\n");
+            } else {
+                outputString.append(currentLine + "\n");
+            }
+        }
         return outputString.toString();
     }
 
-    private String injectToFunc(String functionString, String className, String funcName) {
-        // injecting code to where the first { is found
-        String firstReplacedString = functionString.replaceFirst("\\{", "{ \n" +
-                "        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();\n" +
-                "        outputCreator.addJSON(\""+className+"\", \"Start_\" + \""+funcName+"\");");
-
-        // if return is found in the function then inject before all the returns
-        // else just insert before the last } found
-        String secondReplacedString;
-        if (firstReplacedString.contains("return")) {
-            secondReplacedString = firstReplacedString.replaceAll(
-                    "return",
-                    "outputCreator.addJSON(\""+className+"\", \"End_\" + \""+funcName+"\");\n " +
-                            "       return");
-        } else {
-            int pos = firstReplacedString.lastIndexOf('}');
-             secondReplacedString = firstReplacedString.substring(0,pos) +
-                    "    outputCreator.addJSON(\""+className+"\", \"End_\" + \""+funcName+"\");\n"
-                    + firstReplacedString.substring(pos+1)
-                    + "}";
-        }
-        return secondReplacedString;
-    }
-
-    private String findFunctionName(String functionString) {
-        // regex taken from Stackoverflow post
-        // https://stackoverflow.com/questions/47663648/javascript-regex-getting-function-name-from-string
-        Pattern pattern = Pattern.compile("([a-zA-Z_{1}][a-zA-Z0-9_]+)(?=\\()");
-        Matcher m = pattern.matcher(functionString);
-        if (m.find()) {
-            return m.group(0);
-        } else {
-            throw new RuntimeException("Cannot find function name.");
-        }
-    }
-
     private String findClassName(String javaString) {
-        // regex taken from Stackoverflow post
-        // https://stackoverflow.com/questions/37403641/regex-to-fetch-the-correct-java-class-name
-        Pattern pattern = Pattern.compile("(?<=\\n|\\A)(?:public\\s)?(class|enum|abstract class)\\s([^\\n\\s]*)");
+        Pattern pattern = Pattern.compile(regexForClass);
         Matcher m = pattern.matcher(javaString);
         if (m.find()) {
             String classnName = m.group(0);
@@ -85,6 +123,16 @@ public class Parser {
         }
     }
 
+    private String findFunctionName(String functionString) {
+        Pattern pattern = Pattern.compile(regexForFunc);
+        Matcher m = pattern.matcher(functionString);
+        if (m.find()) {
+            return m.group(0);
+        } else {
+            throw new RuntimeException("Cannot find function name.");
+        }
+    }
+
     private Boolean checkIfInterface(String javaString) {
         // regex taken from Stackoverflow post
         // https://stackoverflow.com/questions/37403641/regex-to-fetch-the-correct-java-class-name
@@ -92,5 +140,4 @@ public class Parser {
         Matcher m = pattern.matcher(javaString);
         return m.find();
     }
-
 }
