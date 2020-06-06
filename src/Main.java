489f40147a7b6ae2e5b22fe3243e501c60270bfc
Michelle Lei <theoriginalmish@gmail.com>
2020-06-05 22:08:01 -0700

    parser almost done, need to inject code before all the returns

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,41 @@ import parser.Parser;
 public class Main {
     public static void main(String[] args) {
         Parser testParser = new Parser();
-        String testString = "public class Main {\n" +
-                "    public static void main(String[] args) {\n" +
-                "        System.out.println(\"Hello world!\");\n" +
+        String testString = "package ast;\n" +
                 "\n" +
+                "import libs.Node;\n" +
+                "\n" +
+                "// NOTE REST CHORD extends BASEKEY\n" +
+                "public abstract class BASEKEY extends Node {\n" +
+                "    private String theNote = null;\n" +
+                "    private String lengths = null;\n" +
+                "    private String octave = null;\n" +
+                "    public String lengthPattern = \"[s|i|q|h|w]+\";\n" +
+                "    public String octavePattern = \"[\\\\-|\\\\+][1-5]\";\n" +
+                "\n" +
+                "\n" +
+                "    //getter\n" +
+                "    public String getLengths() {\n" +
+                "        return lengths;\n" +
                 "    }\n" +
+                "    public String getOctave() {\n" +
+                "        return octave;\n" +
+                "    }\n" +
+                "    public String getTheNote() {\n" +
+                "        return theNote;\n" +
+                "    }\n" +
+                "\n" +
+                "    //setter\n" +
+                "    public void setLengths(String lengths) { this.lengths = lengths; }\n" +
+                "    public void setOctave(String octave) { this.octave = octave; }\n" +
+                "    public void setTheNote(String theNote) { this.theNote = theNote; }\n" +
+                "\n" +
+                "    public int getIndex(String str, String key1, String key2) {\n" +
+                "        int addSignIndex = str.indexOf(key1);\n" +
+                "        int minusSignIndex = str.indexOf(key2);\n" +
+                "        return Math.max(addSignIndex, minusSignIndex);\n" +
+                "    }\n" +
+                "\n" +
                 "}\n";
 
         testParser.parseJavaFile(testString);
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,16 +1,96 @@
 package parser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
-
+    // singleton outputcreator class which will extract when the function gets called
+    // and create a json object
     OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
-    private final String importString = "import parser.OutputCreator; \n";
 
-    public String parseJavaFile(String java) {
+    private final String importString = "import parser.OutputCreator; \n";
+    private final String functionStartString = "";
+    // this regex is taken from Stackoverflow post
+    // https://stackoverflow.com/questions/68633/regex-that-will-match-a-java-method-declaration
+    private String functionRegex = "(?:(?:public|private|protected|static|final|native|synchronized|abstract|transient)+\\s+)+[$_\\w<>\\[\\]\\s]*\\s+[\\$_\\w]+\\([^\\)]*\\)?\\s*\\{?[^\\}]*\\}?";
+
+    public String parseJavaFile(String javaString) {
+        // if the java passed in is an interface then no modification required
+        if (checkIfInterface(javaString)) {
+            return javaString;
+        }
+        String className = findClassName(javaString);
+        // start building the new modified java file as a string
         StringBuilder outputString = new StringBuilder(importString);
 
+        String[] arrOfJavawoFunc = javaString.split(functionRegex);
+        outputString.append(arrOfJavawoFunc[0]);
+        Pattern pattern = Pattern.compile(functionRegex);
+        Matcher m = pattern.matcher(javaString);
+        while (m.find()) {
+            String funcName = findFunctionName(m.group(0));
+            String injectedFunc = injectToFunc(m.group(0), className, funcName);
+            System.out.println(injectedFunc);
+            outputString.append(injectedFunc);
+        }
 
-        System.out.println(outputString.toString());
+        outputString.append(arrOfJavawoFunc[arrOfJavawoFunc.length - 1]);
         return outputString.toString();
+    }
+
+    private String injectToFunc(String functionString, String className, String funcName) {
+        // replacing the first { found
+        String replacedString = functionString.replaceFirst("\\{", "{ \n" +
+                "        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();\n" +
+                "        outputCreator.addString(\""+className+"\", \"Start_\" + \""+funcName+"\");");
+
+        // if return is found in the function then inject before return
+        String secondReplacedString;
+        if (replacedString.matches("return")) {
+            // TODO need to find ALL the returns and inject code before them
+            secondReplacedString = "";
+        } else {
+            int pos = replacedString.lastIndexOf('}');
+             secondReplacedString = replacedString.substring(0,pos) +
+                    "    outputCreator.addString(\""+className+"\", \"End_\" + \""+funcName+"\");\n"
+                    + replacedString.substring(pos+1)
+                    + "}";
+        }
+        return secondReplacedString;
+    }
+
+    private String findFunctionName(String functionString) {
+        // regex taken from Stackoverflow post
+        // https://stackoverflow.com/questions/47663648/javascript-regex-getting-function-name-from-string
+        Pattern pattern = Pattern.compile("([a-zA-Z_{1}][a-zA-Z0-9_]+)(?=\\()");
+        Matcher m = pattern.matcher(functionString);
+        if (m.find()) {
+            return m.group(0);
+        } else {
+            throw new RuntimeException("Cannot find function name");
+        }
+    }
+
+    private String findClassName(String javaString) {
+        Pattern pattern = Pattern.compile("(?<=\\n|\\A)(?:public\\s)?(class|interface|enum|abstract class)\\s([^\\n\\s]*)");
+        Matcher m = pattern.matcher(javaString);
+        if (m.find()) {
+            String classnName = m.group(0);
+            String[] splitStr = classnName.split("\\s+");
+            return splitStr[splitStr.length - 1];
+        } else {
+            throw new RuntimeException("Cannot find class name");
+        }
+    }
+
+    private Boolean checkIfInterface(String javaString) {
+        Pattern pattern = Pattern.compile("(?<=\\n|\\A)(?:public\\s)?(interface)\\s([^\\n\\s]*)");
+        Matcher m = pattern.matcher(javaString);
+        if (m.find())
+           return true;
+        else
+            return false;
+
     }
 
 }
