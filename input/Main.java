8a44cbcdfb60ccc2570ca235a31e12396932d3e8
Michelle Lei <theoriginalmish@gmail.com>
2020-06-07 13:41:06 -0700

    commit before sth is lost

--- input/Main.java
+++ input/Main.java
@@ -4,6 +4,10 @@ import ui.OutputCreator;
 
 import ui.OutputCreator; 
 
+import input.OutputCreator; 
+
+import ui.OutputCreator; 
+
 public class Main {
     String name = "abc";
     public String getName() { 
@@ -12,7 +16,17 @@ public class Main {
  
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addMainStartJSON("Main", "Start_" + "getName");
+ 
+        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
+        outputCreator.addMainStartJSON("Main", "Start_" + "getName");
+ 
+        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
+        outputCreator.addMainStartJSON("Main", "Start_" + "getName");
 
+       outputCreator.addMainEndJSON("Main", "End_" + "getName");
+ outputCreator.writeJSONFile("result.txt");
+       outputCreator.addMainEndJSON("Main", "End_" + "getName");
+ outputCreator.writeJSONFile("result.txt");
        outputCreator.addMainEndJSON("Main", "End_" + "getName");
  outputCreator.writeJSONFile("result.txt");
        outputCreator.addMainEndJSON("Main", "End_" + "getName");
@@ -26,12 +40,24 @@ public class Main {
  
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addMainStartJSON("Main", "Start_" + "asdfasdf");
+ 
+        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
+        outputCreator.addMainStartJSON("Main", "Start_" + "asdfasdf");
+ 
+        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
+        outputCreator.addMainStartJSON("Main", "Start_" + "asdfasdf");
 
          outputCreator.addMainEndJSON("Main", "End_" + "asdfasdf");
 outputCreator.writeJSONFile("result.txt");
          outputCreator.addMainEndJSON("Main", "End_" + "asdfasdf");
 outputCreator.writeJSONFile("result.txt");
+         outputCreator.addMainEndJSON("Main", "End_" + "asdfasdf");
+outputCreator.writeJSONFile("result.txt");
+         outputCreator.addMainEndJSON("Main", "End_" + "asdfasdf");
+outputCreator.writeJSONFile("result.txt");
      } 
+
+
 
 
 
--- input/OutputCreator.java
+++ input/OutputCreator.java
@@ -1,11 +1,9 @@
-package ui;
+package input;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-
-import ui.OutputCreator; 
 
 import ui.OutputCreator; 
 
@@ -18,14 +16,10 @@ public class OutputCreator {
     public static OutputCreator getTheOutputCreator() { 
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "getTheOutputCreator");
- 
-        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
-        outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "getTheOutputCreator");
 
         if (theOutputCreator == null) {
             theOutputCreator = new OutputCreator();
         }
-        outputCreator.addFuncEndJSON("OutputCreator", "End_" + "getTheOutputCreator");
         outputCreator.addFuncEndJSON("OutputCreator", "End_" + "getTheOutputCreator");
         return theOutputCreator;
     }
@@ -33,76 +27,49 @@ public class OutputCreator {
     public static void addMainStartJSON(String className, String functionCalled) { 
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "addMainStartJSON");
- 
-        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
-        outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "addMainStartJSON");
 
         String content = "[{\"classname\":\"" + className + "\",\"function\":\"" + functionCalled + "\"}";
         jsonOutputAry.append(content);
         outputCreator.addFuncEndJSON("OutputCreator", "End_" + "addMainStartJSON");
-        outputCreator.addFuncEndJSON("OutputCreator", "End_" + "addMainStartJSON");
-     } 
-
+    } 
 
 
     public static void addMainEndJSON(String className, String functionCalled) { 
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "addMainEndJSON");
- 
-        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
-        outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "addMainEndJSON");
 
         jsonOutputAry.append(",{\"classname\":\"" + className + "\",\"function\":\"" + functionCalled + "\"}]");
         outputCreator.addFuncEndJSON("OutputCreator", "End_" + "addMainEndJSON");
-        outputCreator.addFuncEndJSON("OutputCreator", "End_" + "addMainEndJSON");
-     } 
-
+    } 
 
 
     public static void addFuncStartJSON(String className, String functionCalled) { 
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "addFuncStartJSON");
- 
-        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
-        outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "addFuncStartJSON");
 
         jsonOutputAry.append(",{\"classname\":\"" + className + "\",\"function\":\"" + functionCalled + "\"}");
         outputCreator.addFuncEndJSON("OutputCreator", "End_" + "addFuncStartJSON");
-        outputCreator.addFuncEndJSON("OutputCreator", "End_" + "addFuncStartJSON");
-     } 
-
+    } 
 
 
     public static void addFuncEndJSON(String className, String functionCalled) { 
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "addFuncEndJSON");
- 
-        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
-        outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "addFuncEndJSON");
 
         jsonOutputAry.append(",{\"classname\":\"" + className + "\",\"function\":\"" + functionCalled + "\"}");
         outputCreator.addFuncEndJSON("OutputCreator", "End_" + "addFuncEndJSON");
-        outputCreator.addFuncEndJSON("OutputCreator", "End_" + "addFuncEndJSON");
-     } 
-
+    } 
 
 
     public static String getJsonOutputAry() { 
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "getJsonOutputAry");
- 
-        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
-        outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "getJsonOutputAry");
 
-        outputCreator.addFuncEndJSON("OutputCreator", "End_" + "getJsonOutputAry");
         outputCreator.addFuncEndJSON("OutputCreator", "End_" + "getJsonOutputAry");
         return jsonOutputAry.toString();
     }
 
     public static void writeJSONFile(String path) { 
-        OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
-        outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "writeJSONFile");
- 
         OutputCreator outputCreator = OutputCreator.getTheOutputCreator();
         outputCreator.addFuncStartJSON("OutputCreator", "Start_" + "writeJSONFile");
 
@@ -121,9 +88,6 @@ public class OutputCreator {
             }
         }
         outputCreator.addFuncEndJSON("OutputCreator", "End_" + "writeJSONFile");
-        outputCreator.addFuncEndJSON("OutputCreator", "End_" + "writeJSONFile");
-     } 
-
-
+    } 
 
 }
