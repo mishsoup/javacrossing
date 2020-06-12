589b7355e1e2f26e97646d9228b375521e52ff76
KevinXu17 <xuzhaozhishu@gmail.com>
2020-06-11 21:18:15 -0700

    1) fixed the PlotlyMain can not read the result file which produce by running the input project. It will read the result file directly inside the input project.

--- src/PlotlyMain.java
+++ src/PlotlyMain.java
@@ -3,13 +3,21 @@ import org.json.JSONArray;
 import reader.JsonReader;
 import writer.JsonWriter;
 
+import java.io.File;
 import java.util.Map;
 
 public class PlotlyMain {
 
     public static void main(String[] args) {
         JsonReader jr = new JsonReader();
-        Map<String, JSONArray> jsonMap =  jr.extract("result.txt");
+        // get the data path
+        String inputDic = "input";
+        File input = new File("input");
+        String[] childPath = input.list();
+        if (childPath[1] == null) throw new RuntimeException("Can not find the input Project path");
+        String dataPath = inputDic + "/" +  childPath[1] + "/result.txt";
+        // read the data
+        Map<String, JSONArray> jsonMap =  jr.extract(dataPath);
         JSONArray parsedResults = jsonMap.get(JsonReader.RESULT_KEY);
 
         PlotlyController plotlyController = new PlotlyController(parsedResults);
